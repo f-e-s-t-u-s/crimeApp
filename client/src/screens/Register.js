6094918681596206c56396f6ef9b7a74fa01b902
Festus <festusgitahik@gmail.com>
2023-09-05 13:12:05 +0300

    added client folders

---
+++ client/src/screens/Register.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+
+const Register = () => {
+  return (
+    <View>
+        <View></View>
+    </View>
+  )
+}
+
+export default Register
