7d84260afff8855f96fe8d5a9bcbe039b0c4a320
Festus <festusgitahik@gmail.com>
2023-09-06 22:01:33 +0300

    added hotlines screen

--- client/src/screens/home/Hotlines.js
+++ client/src/screens/home/Hotlines.js
@@ -1,28 +1,90 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { ActivityIndicator, Modal, ScrollView, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import Card from "../../shared/card";
+import axios from "axios";
+import { API_URL } from "../../services/api";
 
 const Hotlines = () => {
+  const [hotlines, setHotlines] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchHotlines = async () => {
+      setLoading(true)
+      try {
+        const response = await axios.get(`${API_URL}/api/v1/hotlines`);
+        console.log(response.data);
+        setHotlines(response.data);
+        setLoading(false)
+      } catch (error) {
+        console.log(error);
+        setError("An error occurred please try again later");
+      }
+    };
+    fetchHotlines();
+  }, []);
+
   return (
-    <View style={hotline.container}>
-      <View>
-        <Text style={hotline.heading} >
-            Emergency Hotlines
-        </Text>
+    <ScrollView>
+      <View style={hotline.container}>
+      <Modal animationType="fade" transparent={true} visible={loading}>
+          <View style={hotline.modal}>
+            <View style={hotline.modalContainer}>
+              <ActivityIndicator size={"large"} color={"green"} />
+              <Text>Loading.....</Text>
+            </View>
+          </View>
+        </Modal>
+        <View>
+          <Text style={hotline.heading}>Emergency Hotlines</Text>
+        </View>
+
+        <View style={hotline.content}>
+          {hotlines &&
+            hotlines.map((eachHotline) => (
+              <Card
+                key={eachHotline.hotline_id}
+                hotline_name={eachHotline.hotline_name}
+                description={eachHotline.hotline_description}
+                contact={eachHotline.hotline_contact}
+              />
+            ))}
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
 export default Hotlines;
 
 const hotline = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    heading: {
-        color: '#FF0F00',
-        // fontFamily: 'Inter',
-        fontWeight: '700',
-        lineHeight: 36,
-    }
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  modal: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  modalContainer: {
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  heading: {
+    color: "#FF0F00",
+    fontWeight: "700",
+    lineHeight: 36,
+    fontSize: 32,
+    textAlign: "center",
+    padding: 10,
+  },
+  content: {
+    flex: 1,
+    gap: 10,
+  },
 });
--- client/src/shared/card.js
+++ client/src/shared/card.js
@@ -1,16 +1,29 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Linking, StyleSheet, Text, View } from "react-native";
 import React from "react";
 
-const Card = () => {
+const Card = ({ hotline_name, description, contact }) => {
   return (
     <View style={cardStyle.container}>
-      <Text style={cardStyle.header} >Gender Based Violence</Text>
-      <Text style={cardStyle.desc} >
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi
-        veritatis blanditiis nostrum totam iste provident quasi esse nobis
-        dolore quidem.
+      <Text style={cardStyle.header}>{hotline_name}</Text>
+      <Text style={cardStyle.desc}>{description}</Text>
+      <Text
+        onPress={() => {
+          const phoneNumber = `${contact}`;
+          const url = `tel:${phoneNumber}`;
+          Linking.canOpenURL(url)
+            .then((supported) => {
+              if (supported) {
+                return Linking.openURL(url);
+              } else {
+                console.log("Phone call not supported");
+              }
+            })
+            .catch((error) => console.error(error));
+        }}
+        style={cardStyle.contact}
+      >
+        Hotline Number: {contact}
       </Text>
-      <Text style={cardStyle.contact} >Hotline: 0790504636</Text>
     </View>
   );
 };
@@ -18,16 +31,21 @@ const Card = () => {
 export default Card;
 
 const cardStyle = StyleSheet.create({
-    container: {
-        flex: 1,
-        width: 345,
-        top: 1170,
-        left: 46,
-        gap: 7,
-        color: "white",
-        
-    },
-    header: {
-
-    }
+  container: {
+    flex: 1,
+    gap: 12,
+    width: 345,
+    padding: 12,
+    borderColor: "#545F71",
+    borderWidth: 1,
+  },
+  header: {
+    fontWeight: "400",
+    fontSize: 16,
+    lineHeight: 22,
+    color: "#545F71",
+  },
+  contact: {
+    marginBottom: 10,
+  },
 });
